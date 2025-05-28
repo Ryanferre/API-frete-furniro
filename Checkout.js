d19270b0950da7059d523d823512a1d999da24f9
Ryan <ryandsferrer@gmail.com>
2025-05-28 19:09:42 -0300

    explaining code

--- Checkout.js
+++ Checkout.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const haversine_distance_1 = __importDefault(require("haversine-distance"));
+const config_1 = require("./config");
 const server = (0, express_1.default)();
 server.get('/checkout', async (req, res) => {
     const origincepuser = '61618-200';
@@ -11,23 +13,35 @@ server.get('/checkout', async (req, res) => {
         userLocation: { type: null, features: [] },
         LojaLocation: { type: null, features: [] }
     };
+    //buscar geolocalizacao com base no cep
     try {
-        const reqgeopifyuser = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${origincepuser},Caucaia,CE,Brazil&apiKey=2bdbaaa485ca4734af497066f9f2670a`);
-        const reqgeopifyloja = await fetch(`https://api.geoapify.com/v1/geocode/search?text=05407-002,Caucaia,CE,Brazil&apiKey=2bdbaaa485ca4734af497066f9f2670a`);
+        //busca geolocalizacao de cep do usuario
+        const reqgeopifyuser = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${origincepuser},Caucaia,CE,Brazil&apiKey=${config_1.apiKey}`);
+        //busca geolocalizacao de cep da loja
+        const reqgeopifyloja = await fetch(`https://api.geoapify.com/v1/geocode/search?text=05407-002,Caucaia,CE,Brazil&apiKey=${config_1.apiKey}`);
+        //recebe os resultados separadamente e arquivos json
         const datauser = await reqgeopifyuser.json();
         const dataloja = await reqgeopifyloja.json();
+        //pasa os resultados para os objetos separadamente
         LocationLatAndLog.userLocation = datauser;
         LocationLatAndLog.LojaLocation = dataloja;
     }
     catch (error) {
         res.send(error);
     }
+    //quando os dois objetos estiverem prontos, realize a requisicao de distancia
     if (LocationLatAndLog.LojaLocation && LocationLatAndLog.userLocation) {
-        const calcPointAPointB = await fetch(`https://api.geoapify.com/v1/routing?waypoints=${LocationLatAndLog.userLocation.features[0].properties.lat},${LocationLatAndLog.userLocation.features[0].properties.lon}|${LocationLatAndLog.LojaLocation.features[0].properties.lat},${LocationLatAndLog.LojaLocation.features[0].properties.lon}&mode=drive&apiKey=2bdbaaa485ca4734af497066f9f2670a`);
-        const rescalc = await calcPointAPointB.json();
-        const distance = rescalc.features[0].properties.distance;
+        //acessando e definindo latitude de cada objeto
+        const corduser = { lat: LocationLatAndLog.userLocation.features[0].properties.lat, lon: LocationLatAndLog.userLocation.features[0].properties.lon };
+        const cordloja = { lat: LocationLatAndLog.LojaLocation.features[0].properties.lat, lon: LocationLatAndLog.LojaLocation.features[0].properties.lon };
+        //const calcPointAPointB= await fetch(`https://api.geoapify.com/v1/routing?waypoints=${LocationLatAndLog.userLocation.features[0].properties.lat},${LocationLatAndLog.userLocation.features[0].properties.lon}|${LocationLatAndLog.LojaLocation.features[0].properties.lat},${LocationLatAndLog.LojaLocation.features[0].properties.lon}&mode=drive&apiKey=2bdbaaa485ca4734af497066f9f2670a`)
+        //const rescalc= await calcPointAPointB.json()
+        //realizando calculo haversine
+        const distance = (0, haversine_distance_1.default)(corduser, cordloja);
+        //faz o calculo de frente com base na distancia do Ponto A a Ponto B
         if (distance) {
             const calcPrice = Math.max(distance / 1000 * 300);
+            //converte em modeda brasileira(real)
             const convertCoin = calcPrice.toLocaleString('pt-br', {
                 style: 'currency',
                 currency: 'BRL',
---
+++ config.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.apiKey = void 0;
+exports.apiKey = '2bdbaaa485ca4734af497066f9f2670a';
